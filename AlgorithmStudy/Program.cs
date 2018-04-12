ea529493b3899857f38bbf9b70a167c7bcbf6767
mikechan <mikechan@OP21FJ00963A>
2018-04-12 15:28:08 +0800

    ok

--- AlgorithmStudy/Program.cs
+++ AlgorithmStudy/Program.cs
@@ -9,6 +9,10 @@ namespace AlgorithmStudy
     {
         static void Main(string[] args)
         {
+            int[] TestingObject = new int[] { 4,3,2,1,3 };
+            Console.WriteLine(string.Join(",",Sorter.BubbleSort(TestingObject)));
+            Console.WriteLine(string.Join(",", Sorter.SelectionSort(TestingObject)));
+            Console.Read();
         }
     }
 }
---
+++ AlgorithmStudy/Sorter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlgorithmStudy
+{
+    public static class Sorter
+    {
+        public static int[] BubbleSort(int[] intArr)
+        {
+            for (int i = 0; i < intArr.Length-1; i++) //sort the first
+            {
+                for (int n = intArr.Length-1; n >i; n--) //start from right end
+                {
+                    int right = intArr[n];
+                    int left = intArr[n - 1];
+                    if (right<left)
+                    {
+                        //switch left and right
+                        intArr[n - 1] = right;
+                        intArr[n] = left;
+                    }
+                }
+            }
+            return intArr;
+        }
+        public static int[] SelectionSort(int[] intArr)
+        {
+            for (int n = 0; n < intArr.Length; n++)
+            {
+                int now = intArr[n];
+                int smallest = now;
+                int smallestPos = n;
+                for (int i = n+1; i < intArr.Length; i++) //find smallest
+                {
+                    if (intArr[i]<= smallest)
+                    {
+                        smallest = intArr[i];
+                        smallestPos = i;
+                    }
+                }
+                //switch
+                intArr[smallestPos] = now;
+                intArr[n] = smallest;
+            }
+            return intArr;
+        }
+    }
+    
+}
